a06c453c8ece4d24e0e370a28fe5613c5f06904f
BuildTools <unconfigured@null.spigotmc.org>
2025-12-05 16:31:30 +0100

    Visuals10

---
+++ app/home-wrapper.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Suspense } from "react";
+import HomeContent from "./home-content";
+
+export default function HomeWrapper() {
+  return (
+    <Suspense fallback={<div className="text-white p-10">Lädt…</div>}>
+      <HomeContent />
+    </Suspense>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-"use client";
-
-import { Suspense } from "react";
-import HomeContent from "./home-content";
+import HomeWrapper from "./home-wrapper";
 
 export default function Home() {
-  return (
-    <Suspense fallback={<div className="text-white p-10">Lädt…</div>}>
-      <HomeContent />
-    </Suspense>
-  );
+  return <HomeWrapper />;
 }
