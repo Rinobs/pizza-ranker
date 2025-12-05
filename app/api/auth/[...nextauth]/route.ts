7e254cd5e2cc06e2cd13c10e2939670b98f6563b
BuildTools <unconfigured@null.spigotmc.org>
2025-12-05 19:55:47 +0100

    Database 6

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,21 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { SupabaseAdapter } from "@auth/supabase-adapter";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { SupabaseAdapter } from "@auth/supabase-adapter"
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
+    })
   ],
 
   adapter: SupabaseAdapter({
-    url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    secret: process.env.SUPABASE_JWT_SECRET!,  // Wichtig!
+    url: process.env.SUPABASE_URL!,               // NICHT PUBLIC
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY! // SERVICE ROLE KEY
   }),
 
   secret: process.env.NEXTAUTH_SECRET!,
-});
+})
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
